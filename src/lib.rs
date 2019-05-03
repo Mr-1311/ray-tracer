830b36f95943535f94962dab743a01554d53f562
Metin Ur <metinur.1311@gmail.com>
2019-05-03 18:50:10 +0300

    first sphere

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ use vec3::Vec3;
 use ray::Ray;
 
 pub fn ray_tracer() {
-    let nx = 400;
-    let ny = 200;
+    let nx = 800;
+    let ny = 400;
 
     let lover_left_corner = Vec3::new(-2.0, -1.0, -1.0);
     let horizontal = Vec3::new(4.0, 0.0, 0.0);
@@ -33,7 +33,22 @@ pub fn ray_tracer() {
     }
 }
 
+fn hit_sphere( center: &Vec3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - *center;
+    let a = ray.direction.dot(&ray.direction);
+    let b = (2.0 * ray.direction.dot(&oc)) as f64;
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b*b - 4.0*a*c;
+
+    discriminant > 0.0
+}
+
 fn color(r: &Ray) -> Vec3 {
+
+    if hit_sphere(&Vec3::new(0.0,0.0,-1.0), 0.5, r) {
+        return Vec3::new(0.0,1.0,0.0);
+    }
+    
     let unit_direction = Vec3::unit_vector(r.direction);
     let t: f64 = 0.5 * (unit_direction.y + 1.0);
 
