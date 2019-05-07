976a970c403808de849aba3ccf90d3c7303691fd
Metin Ur <metinur.1311@gmail.com>
2019-05-08 01:49:29 +0300

    Positionable camera upgrade

--- src/camera.rs
+++ src/camera.rs
@@ -9,16 +9,20 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f64, aspect: f64) -> Camera {
+    pub fn new(look_from: &Vec3, look_at: &Vec3, vup: &Vec3, vfov: f64, aspect: f64) -> Camera {
         let theta = vfov * std::f64::consts::PI / 180.0;
         let half_height = (theta / 2.0).tan();
         let half_width = aspect * half_height;
 
+        let w = Vec3::unit_vector(*look_from - *look_at);
+        let u = Vec3::unit_vector(Vec3::cross(*vup, w));
+        let v = Vec3::cross(w, u);
+
         Camera {
-            lower_left_corner: Vec3::new(-half_width, -half_height, -1.0),
-            horizontal: Vec3::new(2.0 * half_width, 0.0, 0.0),
-            vertical: Vec3::new(0.0, 2.0 * half_height, 0.0),
-            origin: Vec3::new(0.0, 0.0, 0.0),
+            lower_left_corner: *look_from - half_width * u - half_height * v - w,
+            horizontal: 2.0 * half_width * u,
+            vertical: 2.0 * half_height * v,
+            origin: *look_from,
         }
     }
 
