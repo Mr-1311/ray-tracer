545a4102341d613e1968e9b81bbd02852ac30fdb
Metin Ur <metinur.1311@gmail.com>
2019-05-11 00:05:26 +0300

    added examples

---
+++ examples/ex_dielectric.rs
@@ -0,0 +1,67 @@
+extern crate ray_tracer;
+
+use ray_tracer::camera::Camera;
+use ray_tracer::materials::{
+    dielectric::Dielectric, lambertian::Lambertian, metal::Metal, Material,
+};
+use ray_tracer::reflexible::sphere::Sphere;
+use ray_tracer::reflexible::Reflexible;
+use ray_tracer::reflexible::ReflexibleList;
+use ray_tracer::vec3::Vec3;
+
+fn main() {
+    let nx = 1920;
+    let ny = 1080;
+
+    let sphere_1 = Sphere::new(
+        Vec3::new(0.0, 1.0, -1.0),
+        1.0,
+        Material::Dielectric(Dielectric::new(2.5)),
+    );
+    let sphere_2 = Sphere::new(
+        Vec3::new(0.0, -100.0, -1.0),
+        100.0,
+        Material::Lambertian(Lambertian::new(Vec3::new(0.3, 0.6, 0.7))),
+    );
+    let sphere_3 = Sphere::new(
+        Vec3::new(2.0, 0.5, 0.3),
+        0.5,
+        Material::Metal(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.0)),
+    );
+    let sphere_4 = Sphere::new(
+        Vec3::new(0.0, 2.5, -2.0),
+        0.5,
+        Material::Lambertian(Lambertian::new(Vec3::new(0.1, 0.9, 0.4))),
+    );
+    let sphere_5 = Sphere::new(
+        Vec3::new(-2.0, 0.5, 0.3),
+        0.5,
+        Material::Dielectric(Dielectric::new(2.5)),
+    );
+
+    let list: Vec<Box<dyn Reflexible>> = vec![
+        Box::new(sphere_1),
+        Box::new(sphere_2),
+        Box::new(sphere_3),
+        Box::new(sphere_4),
+        Box::new(sphere_5),
+    ];
+    let world = ReflexibleList::new(list);
+
+    let lookfrom = Vec3::new(0.0, 3.0, 7.0);
+    let lookat = Vec3::new(0.0, 1.5, -1.0);
+    let dist_to_focus = (lookfrom - lookat).length();
+    let aperture = 0.0;
+
+    let cam = Camera::new(
+        &lookfrom,
+        &lookat,
+        &Vec3::new(0.0, 1.0, 0.0),
+        30.0,
+        f64::from(nx) / f64::from(ny),
+        aperture,
+        dist_to_focus,
+    );
+
+    ray_tracer::ray_tracer(&world, &cam, nx, ny, "ex_dielectric.jpg");
+}
---
+++ examples/ex_random.rs
@@ -0,0 +1,106 @@
+extern crate ray_tracer;
+
+use ray_tracer::camera::Camera;
+use ray_tracer::materials::{
+    dielectric::Dielectric, lambertian::Lambertian, metal::Metal, Material,
+};
+use ray_tracer::reflexible::sphere::Sphere;
+use ray_tracer::reflexible::Reflexible;
+use ray_tracer::reflexible::ReflexibleList;
+use ray_tracer::vec3::Vec3;
+
+use rand::Rng;
+
+fn main() {
+    //screen resolution
+    let nx = 1920;
+    let ny = 1080;
+
+    let mut rng = rand::thread_rng();
+
+    let sphere_ground = Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        Material::Lambertian(Lambertian::new(Vec3::new(0.4, 0.6, 0.4))),
+    );
+
+    let mut list: Vec<Box<dyn Reflexible>> = vec![Box::new(sphere_ground)];
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f64 = rng.gen();
+
+            let center = Vec3::new(
+                f64::from(a) + 0.9 * rng.gen::<f64>(),
+                0.2,
+                f64::from(b) + 0.9 * rng.gen::<f64>(),
+            );
+
+            if (center - Vec3::new(4., 0.2, 0.)).length() > 0.9 {
+                //diffuse
+                if choose_mat < 0.5 {
+                    list.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Material::Lambertian(Lambertian::new(Vec3::new(
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                        ))),
+                    )));
+                //metal
+                } else if choose_mat < 0.95 {
+                    list.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Material::Metal(Metal::new(
+                            Vec3::new(
+                                0.5 * (1.0 + rng.gen::<f64>()),
+                                0.5 * (1.0 + rng.gen::<f64>()),
+                                0.5 * (1.0 + rng.gen::<f64>()),
+                            ),
+                            0.1 * rng.gen::<f64>(),
+                        )),
+                    )));
+                } else {
+                    // dielectric
+                    list.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Material::Dielectric(Dielectric::new(1.5)),
+                    )));
+                }
+            }
+        }
+    }
+
+    list.push(Box::new(Sphere::new(
+        Vec3::new(-4., 5., -7.),
+        5.0,
+        Material::Metal(Metal::new(Vec3::new(0.4, 0.2, 0.1), 0.0)),
+    )));
+    list.push(Box::new(Sphere::new(
+        Vec3::new(5.9, 5., -9.),
+        5.0,
+        Material::Dielectric(Dielectric::new(1.5)),
+    )));
+
+    let world = ReflexibleList::new(list);
+
+    let lookfrom = Vec3::new(0.0, 0.5, 7.0);
+    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let dist_to_focus = (lookfrom - lookat).length();
+    let aperture = 0.0;
+
+    let cam = Camera::new(
+        &lookfrom,
+        &lookat,
+        &Vec3::new(0.0, 1.0, 0.0),
+        30.0,
+        f64::from(nx) / f64::from(ny),
+        aperture,
+        dist_to_focus,
+    );
+
+    ray_tracer::ray_tracer(&world, &cam, nx, ny, "ex_random.jpg");
+}
